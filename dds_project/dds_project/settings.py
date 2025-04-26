0ef049a92914f5ef20247d83ddad00c72db2b2d4
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2025-04-26 17:10:03 +0500

    feat[project-main]: Добавить локализацию и форматы дат в настройки проекта

--- dds_project/dds_project/settings.py
+++ dds_project/dds_project/settings.py
@@ -78,7 +78,9 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LANGUAGE_CODE = 'ru-RU'
 TIME_ZONE = 'Europe/Moscow'
+DATE_INPUT_FORMATS = ['%d.%m.%Y']
 USE_I18N = True
+USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
